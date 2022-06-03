34057fdb91de1c175d2169b49e10af4b107e16f4
streq <strequena@gmail.com>
2022-06-03 13:21:14 -0300

    (REFACTOR) cursor.can_put -> PhysicsUtils.anything_overlaps

---
+++ src/global/physics_utils.gd
@@ -0,0 +1,18 @@
+class_name PhysicsUtils
+
+static func anything_overlaps(where: Area2D):
+	# initialize parameters for query
+	var params = Physics2DShapeQueryParameters.new()
+	#assuming single shape area
+	var shape = where.shape_owner_get_shape(0,0)
+	var shape_owner = where.shape_owner_get_owner(0)
+	params.set_shape(shape) 
+	params.transform = shape_owner.global_transform
+	params.collision_layer = where.collision_mask
+	params.collide_with_bodies = true
+	params.collide_with_areas = true
+
+	# do query
+	var space_state = where.get_world_2d().direct_space_state
+	var query_results = space_state.intersect_shape(params)
+	return query_results.size()==0
--- src/pipe/item_cursor.gd
+++ src/pipe/item_cursor.gd
@@ -4,21 +4,7 @@ var can_put := true setget set_can_put
 
 var cursor
 
-func can_put(where: Area2D):
-	# initialize parameters for query
-	var params = Physics2DShapeQueryParameters.new()
-	var shape = where.shape_owner_get_shape(0,0)
-	var shape_owner = where.shape_owner_get_owner(0)
-	params.set_shape(shape) 
-	params.transform = shape_owner.global_transform
-	params.collision_layer = where.collision_mask
-	params.collide_with_bodies = true
-	params.collide_with_areas = true
 
-	# do query
-	var space_state = get_world_2d().direct_space_state
-	var query_results = space_state.intersect_shape(params)
-	return query_results.size()==0
 	
 func _enter_tree():
 	modulate = Color(0,1,0,1)
